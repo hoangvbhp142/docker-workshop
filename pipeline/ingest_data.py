5d3d22611dd9672d98174ecd4101970768ebb1e4
Trần Việt Hoàng <126973129+hoangvbhp142@users.noreply.github.com>
2026-02-18 11:40:01 +0000

    add docker-compose and modify ingest_data

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,76 +1,57 @@
-
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+from config_data import DATASET_CONFIG as DATASET
 
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
-
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-
-def run():    
-    year=2021
-    month=1
-    pg_user='root'
-    pg_password='root'
-    pg_host='localhost'
-    pg_port=5432
-    pg_db='ny_taxi'
-
-    table_name='yellow_taxi_data'
-    chunksize=100000
-
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-
+@click.command()
+@click.option('--year', default=2021, type=int, help='Year to ingest')
+@click.option('--month', default=1, type=int, help='Month to ingest')
+@click.option('--pg_user', default='root', help='PostgreSQL user')
+@click.option('--pg_password', default='root', help='PostgreSQL password')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default=5432, help='PostgreSQL port')
+def run(year, month, pg_user, pg_password, pg_db, pg_host, pg_port):    
     engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
 
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize
-    )
+    for dataset_name, dataset_config in DATASET.items():
+        if dataset_name == "yellow_taxi_data":
+            url = dataset_config["url"].format(year=year, month=month)
+        else:
+            url = dataset_config["url"]
 
-    first=True
+        dtype = dataset_config["dtype"]
+        parse_dates = dataset_config["parse_dates"]
+        table_name = dataset_config["table_name"]
+        chunksize = dataset_config["chunksize"]
 
-    for df_chunk in tqdm(df_iter):
+        print(f'Ingesting {dataset_name} from {url}...')
+            
+        df_iter = pd.read_csv(
+            url,
+            dtype=dtype,
+            parse_dates=parse_dates,
+            iterator=True,
+            chunksize=chunksize
+        )
 
-        if first:
-            df_chunk.head(0).to_sql(
+        first=True
+
+        for df_chunk in tqdm(df_iter):
+            if first:
+                df_chunk.head(0).to_sql(
+                    name=table_name,
+                    con=engine,
+                    if_exists='replace'
+                )
+                first=False
+            df_chunk.to_sql(
                 name=table_name,
                 con=engine,
-                if_exists='replace'
+                if_exists='append'
             )
-            first=False
-
-        df_chunk.to_sql(
-            name=table_name,
-            con=engine,
-            if_exists='append'
-        )
-        print(len(df_chunk))
+            print(len(df_chunk))
 
 if __name__ == '__main__':
     run()
